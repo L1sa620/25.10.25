7df54f7b16c935c79807221b64e4dd76d229a370
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-10-25 23:33:08 +0300

    looking for a three

---
+++ pyth.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+bool pyth(unsigned a, unsigned b, unsigned c);
+bool pyth(unsigned a, unsigned b, unsigned c){
+bool p = a*a == (b*b + c*c);
+p = p || (b*b == (a*a + c*c));
+p = p || (c*c == (a*a + b*b));
+return p;
+}
+int main(){
+using u_t=unsigned;
+u_t a = 0, b = 0, c = 0;
+size_t count = 0;
+while(std::cin >> a){
+	count = count + pyth(a,b,c)?1:0;
+	c = b;
+	b = a;
+}
+if (std::cin.eof()){
+	std::cout << count << "\n";
+} else if (std::cin.fail()){
+	std::cerr << "Input error \n";
+	return 1;
+}
+}
