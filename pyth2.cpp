d57f28e5822c6147467db1306d739d2e7fd4819e
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-10-26 00:18:55 +0300

    A function for the sum has been added. The multiplication check has been placed in a loop

--- pyth2.cpp
+++ pyth2.cpp
@@ -7,10 +7,20 @@ bool max(u_t x, u_t y) {
 const u_t MAX = 999999;
 if (y != 0 && x > MAX / y){
 std::cerr << "Crossing the border";
-return false;
-} 
 return true;
+} 
+return false;
 }
+
+bool sum(u_t x, u_t y) {
+const u_t MAX = 999999;
+if (x > MAX - y){
+std::cerr << "Crossing the border";
+return true;
+} 
+return false;
+}
+
 
 bool pyth(u_t a, u_t b, u_t c){
 bool p = a*a == (b*b + c*c);
@@ -22,15 +32,16 @@ u_t a = 0, b = 0, c = 0;
 size_t count = 0;
 
 while(std::cin >> a){
-	count = count + pyth(a,b,c)?1:0;
+	if (max(a,b)|| max(a,c) || max(b,c)){
+        return 2;
+        }
+
+	count += pyth(a,b,c)?1:0;
 	c = b;
 	b = a;
 }
 
 if (std::cin.eof()){
-	if (max(a,b)|| max(a,c) || max(b,c)){
-	return 2;
-	}
 	std::cout << count << "\n";
 } else if (std::cin.fail()){
 	std::cerr << "Input error \n";
