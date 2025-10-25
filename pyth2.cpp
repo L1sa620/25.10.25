bc2d1fd0c1901ff3211626a6d586ab0abc141599
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-10-26 00:03:54 +0300

    Added a function for checking the border

--- pyth2.cpp
+++ pyth2.cpp
@@ -1,28 +1,39 @@
 #include<iostream>
-bool pyth(unsigned a, unsigned b, unsigned c);
-bool pyth(unsigned a, unsigned b, unsigned c){
+using u_t=unsigned;
+
+bool pyth(u_t a, u_t b, u_t c);
+
+bool max(u_t x, u_t y) {
+const u_t MAX = 999999;
+if (y != 0 && x > MAX / y){
+std::cerr << "Crossing the border";
+return false;
+} 
+return true;
+}
+
+bool pyth(u_t a, u_t b, u_t c){
 bool p = a*a == (b*b + c*c);
-p = p || (b*b == (a*a + c*c));
-p = p || (c*c == (a*a + b*b));
+p = p || (b*b == (a*a + c*c));p = p || (c*c == (a*a + b*b));
 return p;
 }
 int main(){
-using u_t=unsigned;
-const u_t max = 999999;
 u_t a = 0, b = 0, c = 0;
 size_t count = 0;
+
 while(std::cin >> a){
 	count = count + pyth(a,b,c)?1:0;
 	c = b;
 	b = a;
 }
+
 if (std::cin.eof()){
-	if ( a > (max/a)){
+	if (max(a,b)|| max(a,c) || max(b,c)){
 	return 2;
 	}
 	std::cout << count << "\n";
 } else if (std::cin.fail()){
 	std::cerr << "Input error \n";
 	return 1;
-}
+	}
 }
