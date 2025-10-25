a741611e7d659cc25fc21d1e8c4c57967d1cc955
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-10-25 23:21:08 +0300

    count the square

---
+++ sq.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+using u_t=unsigned;
+u_t a = 0;
+size_t count = 0;
+while(std::cin >> a){
+	count = count + a * a;
+}
+if (std::cin.eof()){
+	std::cout << count << "\n";
+} else if (std::cin.fail()){
+	std::cerr << "Input error \n";
+	return 1;
+}
+}
